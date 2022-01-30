647909ad568f8a4431f3fb485390bc59be4f4969
Joshua Horner <lesliewbusiness@gmail.com>
2022-01-30 16:01:53 -0700

    Connect components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,16 +4,30 @@ import Header from '../Header/Header.js';
 import Form from '../Form/Form.js';
 import Results from '../Results/Results.js';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="components-container">
-        <Header />
-        <Form />
-        <Results />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      test: 'Test'
+    }
+  }
+
+  submitTime = (event) => {
+    event.preventDefault();
+    this.setState({ test: 'Noice' })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="components-container">
+          <Header />
+          <Form submitTime={this.submitTime} />
+          <Results results={this.state.test}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Results.css';
 
-const Results = () => {
+const Results = ({results}) => {
   return (
     <div className="results-container">
       <h4 className="results-header">Results</h4>
       <section className="results">
+        {results}
       </section>
     </div>
   )
