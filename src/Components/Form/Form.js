6e0e7c7042c2d691fa41d8d76bd690a05c36f3d0
Joshua Horner <lesliewbusiness@gmail.com>
2022-01-30 14:09:03 -0700

    Render basic components

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+const Form = () => {
+  return (
+    <div>Nice</div>
+  )
+}
+
+export default Form;
---
+++ src/Components/Results/Results.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Results.css';
+
+const Results = () => {
+  return (
+    <div>Results</div>
+  )
+}
+
+export default Results;
