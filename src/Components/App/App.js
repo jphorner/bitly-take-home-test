8569989cda46cbea86a15586d8aa72f0ac9ed67d
Joshua Horner <lesliewbusiness@gmail.com>
2022-01-31 16:05:56 -0700

    Draft minutes calculation

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,8 +12,39 @@ class App extends Component {
     }
   }
 
+  calculateTime = (hour, minutes, day, period) => {
+    let totalTime;
+    totalTime = minutes;
+    if (period === 'PM' && hour !== 12) {
+      let hourAdjusted = hour + 12;
+      console.log('Adjusted: ', hourAdjusted);
+      totalTime += (hourAdjusted * 60);
+    };
+    if (day > 1) {
+      totalTime += ((24 * 60) * day);
+    };
+    totalTime -= (8 * 60);
+    console.log(totalTime);
+  }
+
   submitTime = (event) => {
     event.preventDefault();
+    let time = document.getElementById('timeInput').value;
+    if (time.includes(':') && (time.includes('AM') || time.includes('PM')) && time.includes(',') && time.includes('DAY')) {
+      let splitInput = time.split(',');
+      let splitHour = parseInt(splitInput[0].split(':')[0]);
+      let splitMinutes = parseInt(splitInput[0].split(':')[1].split(' ')[0]);
+      let splitDay = parseInt(splitInput[1].split(' DAY ')[1]);
+      let splitPeriod = splitInput[0].split(':')[1].split(' ')[1];
+      console.log('HOUR: ', splitHour);
+      console.log('MINUTE: ', splitMinutes);
+      console.log('DAY: ', splitDay);
+      console.log('PERIOD: ', splitPeriod);
+      this.calculateTime(splitHour, splitMinutes, splitDay, splitPeriod);
+    } else {
+      console.log('invalid')
+      return;
+    };
     this.setState({ test: 'Noice' })
   }
 
@@ -31,3 +62,9 @@ class App extends Component {
 }
 
 export default App;
+
+// ["12:00 PM, DAY 1",
+// "12:01 PM, DAY 1"]
+// => 241 (240.5 rounded)
+
+// Subtract 480 since race starts at 8:00 AM
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -7,7 +7,7 @@ const Form = ({submitTime}) => {
       <div className="race-time">Race Time</div>
       <form className="race-time-form">
         <div className="time-input-container">
-          <input type="text" className="time-input"></input>
+          <input type="text" className="time-input" id="timeInput"></input>
           <button type="submit" className="time-submit-btn" onClick={submitTime}>Submit</button>
         </div>
       </form>
